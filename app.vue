66239adb4830e493127c2ce9d12941cba79223a4
vinoddotcom <vinod.yadav@vidyamantra.com>
2023-06-13 18:00:26 +0530

    Initial

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Nuxt Image</h3>
+    <nuxt-img
+      :src="'/fighter.svg'"
+      alt="Exam Path finder"
+      format="svg"
+      fetchpriority="low"
+      loading="lazy"
+      decoding="async"
+      width="100"
+      height="100"
+    />
+  <p></p>
+  <h3>With img tag</h3>
+  <img
+    :src="'/fighter.svg'"
+    alt="Exam Path finder"
+    format="svg"
+    fetchpriority="low"
+    loading="lazy"
+    decoding="async"
+    width="109"
+    height="40"
+  />
+
+  </div>
+</template>

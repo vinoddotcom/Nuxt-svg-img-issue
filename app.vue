bb01edc595b85e0e3f12ad116e9f4c66efcb4c99
vinoddotcom <vinod.yadav@vidyamantra.com>
2023-07-03 15:21:48 +0530

    Added extra images

--- app.vue
+++ app.vue
@@ -23,6 +23,27 @@
     width="109"
     height="40"
   />
+  <nuxt-img
+      :src="'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/NewTux.svg/500px-NewTux.svg.png?20081002211004'"
+      alt="Exam Path finder"
+      format="svg"
+      fetchpriority="low"
+      loading="lazy"
+      decoding="async"
+      width="100"
+      height="100"
+    />
+    <img
+      :src="'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/NewTux.svg/500px-NewTux.svg.png?20081002211004'"
+      alt="Exam Path finder"
+      format="svg"
+      fetchpriority="low"
+      loading="lazy"
+      decoding="async"
+      width="100"
+      height="100"
+    />
 
   </div>
 </template>
+
